8542aeba69c30aedd0d1eb2706a8273b3444c85d
Thomas Darimont <thomas.darimont@gmail.com>
2017-06-05 15:33:19 +0200

    List primitive types

---
+++ lesson1/primtivetypes/main.go
@@ -0,0 +1,23 @@
+package main
+
+var (
+	b    bool       = true
+	s    string     = "Hello"
+	i    int        = 32
+	i8   int8       = 127
+	i16  int16      = 32767
+	i32  int32      = 2147483647
+	i64  int64      = 9223372036854775807
+	ii   int        = 9223372036854775807
+	ip   uintptr    = 0
+	by   byte       = 127
+	r    rune       = 'x'
+	f32  float32    = 3.4028235E38
+	f64  float64    = 1.7976931348623157E308
+	c64  complex64  = 1 + 5i
+	c128 complex128 = 1.7976931348623157E308 + 5i
+)
+
+func main() {
+
+}
