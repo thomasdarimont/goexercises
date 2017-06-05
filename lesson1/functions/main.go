02ea92d7056a768952255bfcc9126e3d72dead2b
Thomas Darimont <thomas.darimont@gmail.com>
2017-06-05 15:13:59 +0200

    Multiple named return values

--- lesson1/functions/main.go
+++ lesson1/functions/main.go
@@ -8,7 +8,16 @@ func main() {
 
 	a, b := swap("foo", "bar")
 	fmt.Println(a, b)
+
+	x, y := split(22)
+	fmt.Println(x, y)
 }
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return // naked return
+}
+
 func swap(a string, b string) (string, string) {
 	return b, a
 }
