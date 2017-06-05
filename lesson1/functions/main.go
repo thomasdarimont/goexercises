c9319b4ef7b689fcb68721bbee9ece629b0d2b46
Thomas Darimont <thomas.darimont@gmail.com>
2017-06-05 15:12:04 +0200

    Multiple return values

--- lesson1/functions/main.go
+++ lesson1/functions/main.go
@@ -5,7 +5,14 @@ import "fmt"
 func main() {
 
 	fmt.Println(add(1, 2))
+
+	a, b := swap("foo", "bar")
+	fmt.Println(a, b)
 }
+func swap(a string, b string) (string, string) {
+	return b, a
+}
+
 func add(a, b int) int {
 	return a + b
 }
