fbf37b3f6d2a5421b3992dd1135bf8d7e2459fbe
Thomas Darimont <thomas.darimont@gmail.com>
2017-06-05 16:59:22 +0200

    Handling command-line flags

---
+++ lesson1/flags/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// A string flag that returns a pointer to the value
+	stringFlag := flag.String("name_of_flag",
+		"default value",
+		"description of how this flag should be used")
+
+	// You can also use the typeVar method where you pass a var to the function
+	var intFlag int
+	flag.IntVar(&intFlag, "int_flag_name", 0, "description of how this flag should be used")
+
+	// Needs to be called to get the values
+	flag.Parse()
+	fmt.Println(*stringFlag, intFlag)
+}
